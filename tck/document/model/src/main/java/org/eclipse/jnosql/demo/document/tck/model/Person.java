75ea4a0ce9f33c58b0fe0abccd35f138af1a6711
Maximillian Arruda <dearrudam@gmail.com>
2023-09-18 02:56:35 -0300

    build: setup the tck document model module
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

---
+++ tck/document/model/src/main/java/org/eclipse/jnosql/demo/document/tck/model/Person.java
@@ -0,0 +1,82 @@
+
+/*
+ *    Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *    All rights reserved. This program and the accompanying materials
+ *    are made available under the terms of the Eclipse Public License v1.0
+ *    and Apache License v2.0 which accompanies this distribution.
+ *    The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *    and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *    You may elect to redistribute this code under either of these licenses.
+ *
+ *    Contributors:
+ *
+ *    Maximillian Arruda
+ */
+package org.eclipse.jnosql.demo.document.tck.model;
+
+import jakarta.nosql.Column;
+import jakarta.nosql.Entity;
+import jakarta.nosql.Id;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+public class Person {
+
+    public static Person create(String name, LocalDate birthday) {
+        var person = new Person();
+        person.id = UUID.randomUUID().toString();
+        person.name = name;
+        person.birthday = birthday;
+        return person;
+    }
+
+    @Id
+    private String id;
+
+    @Column
+    private String name;
+
+    @Column
+    private LocalDate birthday;
+
+    /**
+     * Don't use this constructor in production code.
+     */
+    @Deprecated
+    public Person() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
