fd203e92da13e53c83ffab025e22552cc1d4615c
Maximillian Arruda <dearrudam@gmail.com>
2023-09-18 04:30:36 -0300

    fix: fix imports
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- tck/document/tests/src/main/java/org/eclipse/jnosql/demo/document/tck/repository/PageableRepositoryIntegrationTest.java
+++ tck/document/tests/src/main/java/org/eclipse/jnosql/demo/document/tck/repository/PageableRepositoryIntegrationTest.java
@@ -21,8 +21,9 @@ import org.eclipse.jnosql.mapping.Convert;
 import org.eclipse.jnosql.mapping.Converters;
 import org.eclipse.jnosql.mapping.document.DocumentEntityConverter;
 import org.eclipse.jnosql.mapping.document.spi.DocumentExtension;
-import org.eclipse.jnosql.mapping.reflection.EntityMetadataExtension;
+
 import org.eclipse.jnosql.mapping.reflection.Reflections;
+import org.eclipse.jnosql.mapping.spi.EntityMetadataExtension;
 import org.jboss.weld.junit5.auto.AddExtensions;
 import org.jboss.weld.junit5.auto.AddPackages;
 import org.jboss.weld.junit5.auto.EnableAutoWeld;
